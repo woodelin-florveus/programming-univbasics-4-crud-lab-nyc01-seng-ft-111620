1ae795befa57eb4a4fccb691648e33d51558f157
Woodelin Florveus <woodelinflorveus@gmail.com>
2020-10-23 22:27:27 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -13,7 +13,7 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
-    array.unshift(element)
+    array.unshift
 end
 
 def remove_element_from_end_of_array(array)
