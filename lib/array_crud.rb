7c29b9b070fbb60ea1bfe4bb87f9e405842b46bb
Woodelin Florveus <woodelinflorveus@gmail.com>
2020-10-23 22:28:02 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -9,7 +9,7 @@ arr = ["dog","cat","fish","cheetah"]
 end
 
 def add_element_to_end_of_array(array, element)
-    array.shift(element)
+    array << element
 end
 
 def add_element_to_start_of_array(array, element)
